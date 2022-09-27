f354882475f3899301605543c512d340bb908471
Matheus Moura <matheusmouraa@icloud.com>
2022-09-27 20:04:09 -0300

    :construction: Add Main Component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,17 @@
+// Imports
+import React from "react";
+
+// Estilos
+import styles from "./Main.module.css";
+
+// type Props = {};
+
+const Main = () => {
+  return (
+    <div className={styles.MainContainer}>
+      <h1>Main Home</h1>
+    </div>
+  );
+};
+
+export default Main;
