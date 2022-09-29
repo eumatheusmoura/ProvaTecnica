805232ad2c20a6d2c4da90a34e9070a0d1fe91b5
Matheus Moura <matheusmouraa@icloud.com>
2022-09-29 14:15:59 -0300

    :construction: Add Sign Up page

---
+++ src/pages/Cadastrar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Cadastro from "../components/Cadastro/Cadastro";
+import MenuSidebar from "../components/Menu/MenuSidebar";
+
+const Cadastrar = () => {
+  return (
+    <div>
+      <MenuSidebar />
+      <Cadastro />
+    </div>
+  );
+};
+
+export default Cadastrar;
