5a8f629c79cec764c33875f2a655d9ac3d37e0df
Matheus Moura <matheusmouraa@icloud.com>
2022-09-29 14:15:50 -0300

    :construction: Add Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "../components/Main/Main";
+import MenuSidebar from "../components/Menu/MenuSidebar";
+
+const Home = () => {
+  return (
+    <div>
+      <MenuSidebar />
+      <Main />
+    </div>
+  );
+};
+export default Home;
